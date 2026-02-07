ad16d1ca5212408b7d4ca2e621f04fa53c0baaf1
monshelle <anmjka@ewhain.net>
2026-02-08 02:15:22 +0900

    ✨ feat: 마이페이지 알림 설정 조회 및 수정 기능 구현

--- src/main/java/com/aiw/backend/app/model/notification/dto/NotificationDTO.java
+++ src/main/java/com/aiw/backend/app/model/notification/dto/NotificationDTO.java
@@ -1,12 +1,14 @@
 package com.aiw.backend.app.model.notification.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationDTO {
 
     private Long id;
@@ -16,5 +18,13 @@ public class NotificationDTO {
 
     @NotNull
     private Long member;
+
+    //마이페이지 알림 기능 구현
+    private Boolean meetingAlarm;
+    private Boolean deadlineAlarm;
+    private Boolean allAlarm;
+
+    //수정 성공 여부 응답용 필드
+    private Boolean updated;
 
 }
